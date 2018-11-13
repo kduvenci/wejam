306e3ef278af4f84763404ac15cf674d934b431f
Momoko Toyota <toyopet-momoko@hotmail.co.jp>
2018-11-13 14:32:29 +0900

    added _

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_12_091320) do
+ActiveRecord::Schema.define(version: 2018_11_13_040336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2018_11_12_091320) do
     t.bigint "band_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "date"
     t.index ["band_id"], name: "index_jam_sessions_on_band_id"
     t.index ["user_id"], name: "index_jam_sessions_on_user_id"
   end
