ff4d716389ffcc389280932bfd3267e60821d5b8
Ricardo Piras <ricpiras@gmail.com>
2018-11-13 17:18:41 +0900

    Created user controller and completed it, so view works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+  def index
+    @users = Band.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :instrument, :description, :photo)
+  end
 end
