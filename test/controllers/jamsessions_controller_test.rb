af4c140e720354e841515c7f366e927ef779f36b
Momoko Toyota <toyopet-momoko@hotmail.co.jp>
2018-11-13 12:33:25 +0900

    destroyed the jamsession controller and created a jamsessions controller

--- test/controllers/jamsessions_controller_test.rb
+++ test/controllers/jamsessions_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class JamsessionControllerTest < ActionDispatch::IntegrationTest
+class JamsessionsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
