b5eb975d6b102d560076da1e2563400cdde00be9
kemal duvenci <k.duvenci@gmail.com>
2018-11-14 10:57:41 +0900

    geocode part 1

--- app/models/band.rb
+++ app/models/band.rb
@@ -5,4 +5,6 @@ class Band < ApplicationRecord
   validates :name, uniqueness: true, presence: true
   validates :genre, presence: true
   mount_uploader :photo, PhotoUploader
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
