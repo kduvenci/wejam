bb6c09e0ac38b5bf50010425f6d59195cd5a9ceb
Ricardo Piras <ricpiras@gmail.com>
2018-11-15 13:58:56 +0900

    Created links for images to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,20 +19,21 @@ AREAS = %w(Shibuya Shinjuku Meguro Ikebukuro Shin-Okubo)
     password: "123456")
 end
 
-counter = 0
-photos = ["avatar_1.jpg", "avatar_2.jpg", "avatar_3.jpg", "avatar_4.jpg", "avatar_5.jpg", "avatar_6.jpg"]
+# counter = 0
+photos = url("https://www.dropbox.com/s/u7mtx85osfzm5fe/avatar%201.jpg?dl=0", "https://www.dropbox.com/s/adc56qitf301tff/avatar%202.jpg?dl=0", "https://www.dropbox.com/s/fio2cfexka331br/avatar%203.jpg?dl=0", "https://www.dropbox.com/s/y5yy3dz6bx5fy35/avatar%204.jpg?dl=0", "https://www.dropbox.com/s/8t473353m58rlh7/avatar%205.jpg?dl=0", "https://www.dropbox.com/s/jjq1tw715zp6157/avatar%206.jpg?dl=0")
 6.times do
   instrument = Faker::Music.instrument
-  Band.create(
+  band = Band.new(
     name: Faker::RockBand.name,
     genre: Faker::Music.genre,
     description: "We need a #{instrument} player to join and help us build our future album, '#{Faker::Music.album }'.",
     instrument: instrument,
     address: AREAS.sample,
-    photo: photos[counter],
     user: User.all.sample
     )
-  counter += 1
+  band.remote_photo_url = photos[counter]
+  band.save!
+  # counter += 1
 end
 sample_user = User.first
 sample_band = Band.first
