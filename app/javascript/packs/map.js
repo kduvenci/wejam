c2454cb45bac44141e148d31c274fb9dea65833d
Amanda Dilworth <amandadilworth@yahoo.com>
2018-12-10 14:14:14 +0900

    made map js file

---
+++ app/javascript/packs/map.js
@@ -0,0 +1,8 @@
+handler = Gmaps.build('Google');
+handler.buildMap({ provider: {}, interna;: {id: 'map'}}, function() {
+  markers.handler.addMarkers([
+  {
+    marker.lng, marker.lat
+    ])
+  }
+})
